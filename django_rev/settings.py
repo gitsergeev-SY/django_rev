64663afd2acb864f786f0dfb4864652590e5a0e9
Sergey <sergeev191419@gmail.com>
2025-10-02 07:44:53 +0300

    зробили корзину, додали заголовки

--- django_rev/settings.py
+++ django_rev/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -124,6 +125,8 @@ STATICFILES_DIRS = [
 ] 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join('BASE_DIR', 'media')
+
+CART_SESSION_ID = 'cart'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
